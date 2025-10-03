a058177e0c17f6e755221f8d0be283d8d011c4b9
Santiago Reyes <santiago.reyes@platinum.net.co>
2025-10-03 09:03:35 -0500

    terminada seccion 15 - Profundizacion de formularios reactivos y inicio de seccion 13 uso de pipes persoonalizadas

--- src/app/country/core/services/country.service.ts
+++ src/app/country/core/services/country.service.ts
@@ -1,6 +1,6 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
+import {combineLatest, Observable, of} from 'rxjs';
 import {CountryInterface} from '../interfaces/country.interface';
 
 @Injectable({
@@ -21,5 +21,16 @@ export class CountryService {
     if (!region) return of([]);
     const url = `${this.apiUrl}region/${region}?fields=cca3,name,borders`
     return this.http.get<CountryInterface[]>(url);
+  }
+
+  getCountryByAlphaCode(alphaCode: string):Observable<CountryInterface> {
+    const url = `${this.apiUrl}alpha/${alphaCode}?fields=cca3,name,borders`
+    return this.http.get<CountryInterface>(url);
+  }
+
+  getCountryNamesByCodeArray(countryCodes: string[]):Observable<CountryInterface[]> {
+    if (!countryCodes) return of([]);
+    const countriesRequests: Observable<CountryInterface>[] = countryCodes.map((code) => this.getCountryByAlphaCode(code)) ?? [];
+    return combineLatest(countriesRequests);
   }
 }
--- src/app/country/pages/country-page/country-page.ts
+++ src/app/country/pages/country-page/country-page.ts
@@ -1,11 +1,65 @@
-import { Component } from '@angular/core';
+import {Component, effect, inject, signal} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {JsonPipe} from '@angular/common';
+import {CountryService} from '../../core/services/country.service';
+import {CountryInterface} from '../../core/interfaces/country.interface';
+import {filter, switchMap, tap} from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [
+    JsonPipe,
+    ReactiveFormsModule
+  ],
   templateUrl: './country-page.html',
   styleUrl: './country-page.css'
 })
 export class CountryPage {
+  private fb = inject(FormBuilder);
+  private countryService = inject(CountryService);
+
+  regions = signal(this.countryService.regions)
+  countryByRegion = signal<CountryInterface[]>([])
+  borders = signal<CountryInterface[]>([])
+
+  onFormChanged = effect((onCleanup) => {
+    const regionSubscription = this.onRegionChanged();
+    const countrySubscription = this.onCountryChanged();
+    onCleanup(() => {
+      regionSubscription.unsubscribe();
+      countrySubscription.unsubscribe();
+      console.log('cleanup');
+    })
+  })
+
+  onRegionChanged() {
+    return this.myFormCountry.get('region')!.valueChanges.pipe(
+      tap(() => this.myFormCountry.patchValue({country: '', border: ''})),
+      switchMap(region => this.countryService.getCountriesByRegion(region!)),
+    )
+      .subscribe(countries => {
+        this.countryByRegion.set(countries);
+      })
+  }
+
+  onCountryChanged() {
+    return this.myFormCountry.get('country')!.valueChanges.pipe(
+      tap(() => this.myFormCountry.patchValue({border: ''})),
+      filter(alphaCode => alphaCode!.length > 0),
+      switchMap(alphaCode => this.countryService.getCountryByAlphaCode(alphaCode!)),
+      switchMap(country => this.countryService.getCountryNamesByCodeArray(country.borders!)),
+    )
+      .subscribe(countries => {
+        this.borders.set(countries);
+      })
+  }
+
+  myFormCountry = this.fb.group({
+    region: ['',Validators.required],
+    country: ['',Validators.required],
+    border: ['',Validators.required]
+  })
+
+
 
 }
